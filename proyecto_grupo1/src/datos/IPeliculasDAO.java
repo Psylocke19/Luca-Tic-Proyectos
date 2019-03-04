0ea98c6a47fa19c99fd39c4775ad42381f0a690f
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-04 12:15:22 +0100

    Interfaz de Peliculas en la capa Datos

---
+++ proyecto_grupo1/src/datos/IPeliculasDAO.java
@@ -0,0 +1,15 @@
+package datos;
+
+import modelo.Peliculas;
+
+public interface IPeliculasDAO {
+	public void addPeliculas(Peliculas pelicula);
+	public void modificarPeliculas(Peliculas pelicula);
+    public void eliminarPeliculas(Peliculas pelicula);
+    public void listarPeliculas();
+    public void listarPorMasVistas();
+    public void listarPorMasValoradas();
+    public boolean comprobacionPeliculaDuplicada(String nombre,int anio);
+    public Peliculas buscarPeliculas(String nombre);
+	
+}
