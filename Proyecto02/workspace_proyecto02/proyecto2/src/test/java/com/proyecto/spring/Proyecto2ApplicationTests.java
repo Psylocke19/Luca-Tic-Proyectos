798bfeca800f61e9361194bb2b52fd38b96930cd
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-20 16:08:19 +0100

    Añadidas propiedades en el aplication properties

--- Proyecto02/workspace_proyecto02/proyecto2/src/test/java/com/proyecto/spring/Proyecto2ApplicationTests.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/test/java/com/proyecto/spring/Proyecto2ApplicationTests.java
@@ -1,16 +1,28 @@
 package com.proyecto.spring;
 
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.proyecto.spring.control.Control;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Proyecto2ApplicationTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(Proyecto2ApplicationTests.class);
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		logger.info("@BeforeClass: Al inicio de las pruebas");
+	}
 }
