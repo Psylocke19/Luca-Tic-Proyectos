0b05b226bdb021706bdd829c47accb7661a402f1
Psylocke19 <pau.santiago19@gmail.com>
2019-03-20 12:21:34 +0100

    Método appProvincias en Servicios

--- Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/service/Servicios.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/service/Servicios.java
@@ -1,5 +1,28 @@
 package com.proyecto.spring.service;
 
-public class Servicios {
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.model.Provincia;
+import com.proyecto.spring.dao.DAOProvincias;
+
+@Service
+@Transactional
+public class Servicios implements IServicios {
+	
+	@Autowired
+	private DAOProvincias datosprovincia; 
+	
+	/**
+	 * Con este método llamamops a la capa inmediatamente a la capa siguiente
+	 * @author Grupo 1
+	 * @param p
+	 */
+	public void addProvincia (Provincia p) {
+		datosprovincia.addProvincia(p); 
+		
+	}
 
 }
