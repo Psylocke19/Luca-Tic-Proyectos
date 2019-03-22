4020d3fd1480487b115e40791ab7939a9deafb77
mrocio <mrociocamachomartinez@hotmail.com>
2019-03-22 11:50:08 +0100

    Prueba unitaria addProvincia

--- Proyecto02/workspace_proyecto02/proyecto2/src/test/java/com/proyecto/spring/Proyecto2ApplicationTests.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/test/java/com/proyecto/spring/Proyecto2ApplicationTests.java
@@ -1,28 +1,47 @@
 package com.proyecto.spring;
 
 
-import org.junit.BeforeClass;
+
+
+import static org.junit.Assert.assertTrue;
+
+
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.proyecto.spring.control.Control;
+import com.proyecto.spring.dao.IProvincia;
+import com.proyecto.spring.model.Provincia;
+import com.proyecto.spring.service.IServicios;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Proyecto2ApplicationTests {
 
 	private static final Logger logger = LoggerFactory.getLogger(Proyecto2ApplicationTests.class);
+	@Autowired
+	private IProvincia provincias;
+	@Autowired
+	private IServicios servicios;
+
+
 	
 	@Test
-	public void contextLoads() {
-	}
-
-	@BeforeClass
-	public static void onceExecutedBeforeAll() {
-		logger.info("@BeforeClass: Al inicio de las pruebas");
+	public void addProvinciaTest() {
+		logger.info("Añadiendo Provincia vacía.");
+		int numProvInicial = provincias.findAll().size(); 
+		logger.info("Inicial"+numProvInicial);
+		Provincia p = new Provincia(100, "Borrame");
+		servicios.addProvincia(p);
+		int numProvFinal = provincias.findAll().size(); 
+		logger.info("Final"+numProvFinal);
+		assertTrue((numProvInicial+1)==numProvFinal);
+		servicios.eliminarProvincias(58);
+		assertTrue((numProvInicial)==numProvFinal-1);
 	}
 }
