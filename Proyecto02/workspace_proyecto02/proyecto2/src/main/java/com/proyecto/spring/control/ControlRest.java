24505cda2bdd23e47404a1ad54a6378b8c79b71d
mrocio <mrociocamachomartinez@hotmail.com>
2019-03-25 16:15:32 +0100

    Añadido metodo editarContacto de Rest comentado

--- Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/control/ControlRest.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/control/ControlRest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,20 @@ public class ControlRest {
 		
 	}
 	
+
+	/**
+	 * Metodo que llama a la capa servicios y que devuelve un Objeto Contacto
+	 * @param id
+	 * @param Contacto c
+	 * @return
+	 */
+	@GetMapping("/editarContacto/{id}")
+	public Contacto editarContacto(@PathVariable int id, Contacto c) {
+		
+		
+		return servicios.editarContacto();
+	
+	
+}
 
 }
