5eb97edcf8f7fbc347c7fbbf4bb99a8e45817cb6
JoseManuel1462 <jose_ap_1992@hotmail.com>
2019-03-26 08:52:14 +0100

    se crean los rest de control addcontacto

--- Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/control/ControlRest.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/control/ControlRest.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.proyecto.spring.model.Contacto;
+
 import com.proyecto.spring.service.IServicios;
 
 @RestController
@@ -16,20 +20,36 @@ public class ControlRest {
 
 	@Autowired
 	private IServicios servicios;
-	
+
 	/**
 	 * Metodo que retorna una lista de Contactos llamado de la capa servicios
 	 * 
 	 * @return
 	 */
 	@GetMapping("/mostrarContacto")
-	public List<Contacto> mostrarContacto(){
-		
-		
+	public List<Contacto> mostrarContacto() {
+
 		return servicios.mostrarContactos();
-		
-		
+
+	}
+
+	
+	@GetMapping("/addContacto")
+	public ModelAndView addContacto() throws Exception {
+
+		// Una vez se haya annadido nos redirigimos a la pagina inicial de provincias
+		ModelAndView model = new ModelAndView("addContacto");
+		model.addObject("claveContactoRest", new Contacto());
+		return model;
+
 	}
 	
-
+	@PostMapping("/addContacto")
+	public Contacto addContactoPost(@ModelAttribute Contacto c) throws Exception {
+		
+		// Le pasamos el objeto a la parte de servicios
+		servicios.addContacto(c);
+		
+		return c;
+	}
 }
