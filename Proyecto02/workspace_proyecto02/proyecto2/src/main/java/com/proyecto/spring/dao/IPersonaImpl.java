97f1e583e6b3e84d2f71c2868792b5550d7bba86
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-26 09:39:31 +0100

    Metodo buscarPersona implementado

--- Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/dao/IPersonaImpl.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/dao/IPersonaImpl.java
@@ -40,7 +40,7 @@ public class IPersonaImpl implements IPersonaCustom {
 	
 	@Override
 	public Persona buscarPersona(Persona p) {
-		/*
+		
 		Query query = entityManager.createNativeQuery(
 				"FROM Persona WHERE nombre=?, apellido1=?, apellido2=?,dni=?,fechanacimiento=?", Persona.class);
 		query.setParameter(1, p.getNombre());
@@ -48,10 +48,8 @@ public class IPersonaImpl implements IPersonaCustom {
 		query.setParameter(3, p.getApellido2());
 		query.setParameter(4, p.getDni());
 		query.setParameter(5, p.getFechanacimiento());
-		return (Persona) query.getSingleResult();*/
-		
-		//ASR
-		return new Persona();
+		return (Persona) query.getSingleResult();
+
 	} 
 
 }
