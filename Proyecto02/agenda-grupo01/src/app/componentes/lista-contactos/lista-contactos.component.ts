04e7884e6cab0cccb9cfd51c43386fe3d184c8d9
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-27 16:36:47 +0100

    Implementado el ts de listarContacto con los metodos de eliminar y mostrarContactos

--- Proyecto02/agenda-grupo01/src/app/componentes/lista-contactos/lista-contactos.component.ts
+++ Proyecto02/agenda-grupo01/src/app/componentes/lista-contactos/lista-contactos.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+//Nuevo
+import { Router } from '@angular/router';
+
+import { Contacto } from '../../model/contacto';
+import { ServiciosService } from '../../servicios/servicios.service';
+
+
 @Component({
   selector: 'app-lista-contactos',
   templateUrl: './lista-contactos.component.html',
@@ -7,9 +15,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaContactosComponent implements OnInit {
 
-  constructor() { }
+  contactos : Contacto[];
 
+  constructor(private router: Router, private service: ServiciosService) { }
+
+  //Iniciamos el componente mostando la lista de Contactos, llamamos a la clase Servicios para realizar la peticion
   ngOnInit() {
+    this.service.getContactos()
+    .subscribe( data => {
+      this.contactos = data;
+    });
   }
+
+  //Metodo que le pasamos un contacto por parametros y llama a la clase Servicios para realizar la peticion  de eliminar
+  eliminarContacto(contacto: Contacto): void {
+    this.service.eliminarContacto(contacto)
+      .subscribe( data => {
+        this.contactos = this.contactos.filter(u => u !== contacto);
+        //Lo de arriba filtra la lista para que si el contacto se ha eliminado desaparezca
+      })
+  };
 
 }
