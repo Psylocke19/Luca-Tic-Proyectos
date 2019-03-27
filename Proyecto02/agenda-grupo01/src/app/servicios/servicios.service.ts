35cc8475c1d66a053fff085ba2851f36a80f2c86
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-27 16:11:42 +0100

    Configurada la clase Servicios y añadido el metodo mostrarContactos

--- Proyecto02/agenda-grupo01/src/app/servicios/servicios.service.ts
+++ Proyecto02/agenda-grupo01/src/app/servicios/servicios.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
+//Nuevo
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contacto } from '../model/contacto';
+
+//Nuevo
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServiciosService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  //Url original para construir en base
+  private url = 'http://localhost:8080/';
+
+  public getContactos() {
+    return this.http.get<Contacto[]>(this.url + "mostrarContacto");
+  }
 }
