6e4ecebac0745241f7d07a3c7ae185f347522f6b
JoseManuel1462 <jose_ap_1992@hotmail.com>
2019-03-27 16:11:20 +0100

    subida modelo contacto

--- Proyecto02/agenda-grupo01/src/app/model/contacto.ts
+++ Proyecto02/agenda-grupo01/src/app/model/contacto.ts
@@ -1,2 +1,12 @@
+import { Persona } from "./persona";
+import { Telefono } from "./telefono";
+import { Direccion } from "./direccion";
+
 export class Contacto {
+
+    persona : Persona
+	telefonomovil:Telefono
+    direccion:Direccion
+    telefono: Telefono[];
+
 }
