c4c10e3e4b6d050d3d39f878a6a2c4d189050a4a
JoseManuel1462 <jose_ap_1992@hotmail.com>
2019-03-27 15:59:45 +0100

    direccion model angular

--- Proyecto02/agenda-grupo01/src/app/model/direccion.ts
+++ Proyecto02/agenda-grupo01/src/app/model/direccion.ts
@@ -1,2 +1,18 @@
+import { Persona } from './persona';
+import { Provincia } from './provincia';
+
+
 export class Direccion {
+    
+
+	codpostal: String;
+
+	direccion: String;
+
+	localidad : String;
+
+	persona : Persona;
+
+	provincia : Provincia ;
+    
 }
