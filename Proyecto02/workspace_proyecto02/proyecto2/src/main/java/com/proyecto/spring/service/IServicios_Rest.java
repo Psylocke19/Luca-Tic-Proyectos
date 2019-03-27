1ae44f0fa5d28d02fa80234604ff2cd21c7ee185
Raspi98 <jpramirezquintana98@gmail.com>
2019-03-27 15:44:26 +0100

    Arreglados fallos de merge del proyecto de eclipse

--- Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/service/IServicios_Rest.java
+++ Proyecto02/workspace_proyecto02/proyecto2/src/main/java/com/proyecto/spring/service/IServicios_Rest.java
@@ -10,12 +10,11 @@ public interface IServicios_Rest {
 	public Contacto addContacto(Contacto c) ;
 	public ArrayList<Contacto> mostrarContactos();
 	public boolean borrarId(int idContacto) ;
-<<<<<<< HEAD
-	public Contacto editarContacto(Contacto c);
+	//public Contacto editarContacto(Contacto c);
 	public List buscarPorProvincia(String provincia) ;
-=======
+
 	/**public Contacto editarContacto(Contacto c);
 	public Contacto BuscadorContacto(int id);
 	*/
->>>>>>> 2b17dba0e627661ea165b9d3c5b381ec236e28d1
+
 }
